03ba46081414c0b8c85e40dc9826495540dd5752
dd <1298154056@qq.com>
2022-11-22 10:17:13 +0800

    修订

--- src/main/java/swine_search/domain/Chromosome.java
+++ src/main/java/swine_search/domain/Chromosome.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -16,12 +15,12 @@ public class Chromosome implements Serializable {
     @TableId
     private Integer chrId;
 
+    private String chrName;
+
     private String chrNum;
 
     private String chrSource;
 
-
-    @Serial
     private static final long serialVersionUID = 1L;
 
     @Override
@@ -37,6 +36,7 @@ public class Chromosome implements Serializable {
         }
         Chromosome other = (Chromosome) that;
         return (this.getChrId() == null ? other.getChrId() == null : this.getChrId().equals(other.getChrId()))
+            && (this.getChrName() == null ? other.getChrName() == null : this.getChrName().equals(other.getChrName()))
             && (this.getChrNum() == null ? other.getChrNum() == null : this.getChrNum().equals(other.getChrNum()))
             && (this.getChrSource() == null ? other.getChrSource() == null : this.getChrSource().equals(other.getChrSource()));
     }
@@ -46,6 +46,7 @@ public class Chromosome implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((getChrId() == null) ? 0 : getChrId().hashCode());
+        result = prime * result + ((getChrName() == null) ? 0 : getChrName().hashCode());
         result = prime * result + ((getChrNum() == null) ? 0 : getChrNum().hashCode());
         result = prime * result + ((getChrSource() == null) ? 0 : getChrSource().hashCode());
         return result;
@@ -58,8 +59,9 @@ public class Chromosome implements Serializable {
         sb.append(" [");
         sb.append("Hash = ").append(hashCode());
         sb.append(", chrId=").append(chrId);
+        sb.append(", chrName=").append(chrName);
         sb.append(", chrNum=").append(chrNum);
-        sb.append(", chrSourse=").append(chrSource);
+        sb.append(", chrSource=").append(chrSource);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
