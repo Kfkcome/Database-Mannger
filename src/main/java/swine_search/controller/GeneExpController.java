35ca572717bedf8111dc8511ddf4ded676a0b39d
dd <1298154056@qq.com>
2022-11-28 15:20:15 +0800

    修订

---
+++ src/main/java/swine_search/controller/GeneExpController.java
@@ -0,0 +1,55 @@
+package swine_search.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import swine_search.constant.MyBatisConstants;
+import swine_search.domain.Gene;
+import swine_search.domain.GeneExp;
+import swine_search.service.GeneExpService;
+import swine_search.util.ResponseDataUtils;
+import swine_search.util.ResponseResult;
+
+import java.util.List;
+
+/**
+ * @Author 刘铭康
+ * @Date 2022/11/14
+ */
+@RestController
+@RequestMapping
+public class GeneExpController {
+    @Autowired
+    GeneExpService geneExpService;
+
+    @GetMapping("/id/{id}")
+    public ResponseResult getGeneById(@PathVariable Integer id) {
+        GeneExp geneExp = geneExpService.getById(id);
+        return ResponseDataUtils.getResponseResult(geneExp.getClass(), geneExp);
+    }
+
+    @GetMapping("/all")
+    public ResponseResult getAllGene() {
+        List<GeneExp> list = geneExpService.list();
+        return ResponseDataUtils.getResponseResult(Gene.class, list);
+    }
+
+    @GetMapping("/page/{current}")
+    public ResponseResult getCdsBasePage(@PathVariable Integer current) {
+        Page<Gene> genePage = new Page<>(current, MyBatisConstants.PAGE_SIZE);
+        List<Gene> records = genePage.getRecords();
+        return ResponseDataUtils.getResponseResult(Gene.class, records);
+    }
+
+    @GetMapping("/page-count")
+    public ResponseResult getPageCount() {
+        Long totalRows = geneExpService.count();
+        Long pageCount = totalRows % MyBatisConstants.PAGE_SIZE == 0 ? totalRows / MyBatisConstants.PAGE_SIZE : totalRows / MyBatisConstants.PAGE_SIZE + 1;
+        // TODO: 2022/11/16 基于路径？？？ 动态获取keyName
+        return ResponseDataUtils.getResponseResult("pageCount", pageCount);
+    }
+}
+
